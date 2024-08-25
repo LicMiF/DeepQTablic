b2b9838bf32cf0f419c870e836bf8e9bd3058eba
Filip Milic <cofi.milic@gmail.com>
2024-08-25 23:06:06 +0200

    Iterate over gamma

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import random
 
 # Number of episodes
 EPISODES = 50000
-SAVE_FREQ = 100
+SAVE_FREQ = 1000
 
 # Update frequency
 UPDATE_FREQ = 5
@@ -28,88 +28,90 @@ DEVICE="cpu"
 # Checkpoint path
 chckPointPath=None
 if __name__ == '__main__':
-    random.seed(0)
-    agent=DQNAgent(GAMMA,device=DEVICE)
-    player = QPlayer(agent)
-    epsilon = EPSILON_START
-    startEpisode=1
-    start = time.time()
 
-    if chckPointPath:
-        trainDict=agent.loadCheckpoint(chckPointPath)
-        startEpisode=trainDict['episode']
-        epsilon=trainDict['epsilon']
-        EPISODES=trainDict['EPISODES']
-        SAVE_FREQ=trainDict['SAVE_FREQ']
-        UPDATE_FREQ=trainDict['UPDATE_FREQ']
-        SWITCH_FREQ=trainDict['SWITCH_FREQ']
-        EPSILON_END=trainDict['EPSILON_END']
-        EPSILON_DECAY=trainDict['EPSILON_DECAY']
-        elapsedTime=trainDict['elapsedTime']
+    for GAMMA in [0,0.5,0.75,0.85]:
+        random.seed(0)
+        agent=DQNAgent(GAMMA,device=DEVICE)
+        player = QPlayer(agent)
+        epsilon = EPSILON_START
+        startEpisode=1
+        start = time.time()
 
-        start=start-elapsedTime
+        if chckPointPath:
+            trainDict=agent.loadCheckpoint(chckPointPath)
+            startEpisode=trainDict['episode']
+            epsilon=trainDict['epsilon']
+            EPISODES=trainDict['EPISODES']
+            SAVE_FREQ=trainDict['SAVE_FREQ']
+            UPDATE_FREQ=trainDict['UPDATE_FREQ']
+            SWITCH_FREQ=trainDict['SWITCH_FREQ']
+            EPSILON_END=trainDict['EPSILON_END']
+            EPSILON_DECAY=trainDict['EPSILON_DECAY']
+            elapsedTime=trainDict['elapsedTime']
 
-        random.setstate(trainDict['rngState'])
+            start=start-elapsedTime
 
-    for episode in range(startEpisode, EPISODES+1):
-        deck= Tablic.getShuffledDeck()
-        game = Tablic(deck=deck)
-        game_actions = [[],[]]
-        game_rewards = [[],[]]
-        game_valid_actions = [[],[]]
+            random.setstate(trainDict['rngState'])
 
-        if (episode > SWITCH_FREQ and episode % SWITCH_FREQ == 0):
-            agent.updateTarget()
+        for episode in range(startEpisode, EPISODES+1):
+            deck= Tablic.getShuffledDeck()
+            game = Tablic(deck=deck)
+            game_actions = [[],[]]
+            game_rewards = [[],[]]
+            game_valid_actions = [[],[]]
 
-        while not game.isTerminal:
-            current_player = game.currentPlayer
+            if (episode > SWITCH_FREQ and episode % SWITCH_FREQ == 0):
+                agent.updateTarget()
 
-            all_state_actions = game.allValidStateActions(game.table,game.hands[game.currentPlayer],game.getGameStateRepresentation())
-            game_valid_actions[current_player].append(all_state_actions)
-            game_rewards[current_player].append(game.rewards[current_player])
+            while not game.isTerminal:
+                current_player = game.currentPlayer
 
-            if random.random() < epsilon:
-                played_card, played_take = player.playerRandomPlay(game)
-            else:
-                played_card, played_take = player.playerPolicyPlay(game)
+                all_state_actions = game.allValidStateActions(game.table,game.hands[game.currentPlayer],game.getGameStateRepresentation())
+                game_valid_actions[current_player].append(all_state_actions)
+                game_rewards[current_player].append(game.rewards[current_player])
 
-            state_action = np.concatenate((game.getActionRepresentation(played_card,played_take),game.getGameStateRepresentation()))
-            game.playCard(played_card, list(played_take))
-            game_actions[current_player].append(state_action)
+                if random.random() < epsilon:
+                    played_card, played_take = player.playerRandomPlay(game)
+                else:
+                    played_card, played_take = player.playerPolicyPlay(game)
 
-        for current_player in range(2):
-            game_rewards[current_player].append(game.rewards[current_player])
-            game_valid_actions[current_player].append([])
-            for i in range(len(game_actions[current_player])):
-                action = game_actions[current_player][i]
-                reward = game_rewards[current_player][i+1] - game_rewards[current_player][i]
-                valid_actions = game_valid_actions[current_player][i+1]
-                player.agent.remember(action, reward, valid_actions)
-        
-        if episode % UPDATE_FREQ == 0:
-            print(f"Time needed for {episode} episodes: {time.time()-start}")
-            agent.backward()
+                state_action = np.concatenate((game.getActionRepresentation(played_card,played_take),game.getGameStateRepresentation()))
+                game.playCard(played_card, list(played_take))
+                game_actions[current_player].append(state_action)
+
+            for current_player in range(2):
+                game_rewards[current_player].append(game.rewards[current_player])
+                game_valid_actions[current_player].append([])
+                for i in range(len(game_actions[current_player])):
+                    action = game_actions[current_player][i]
+                    reward = game_rewards[current_player][i+1] - game_rewards[current_player][i]
+                    valid_actions = game_valid_actions[current_player][i+1]
+                    player.agent.remember(action, reward, valid_actions)
+            
+            if episode % UPDATE_FREQ == 0:
+                print(f"Time needed for {episode} episodes: {time.time()-start}")
+                agent.backward()
 
 
-        if episode % SAVE_FREQ == 0:
-            print(f"Episode {episode} saved.")
-            agent.saveModelsParams(f"models/minimalModelParams/g1{int(GAMMA*100)}e{episode}")
+            if episode % SAVE_FREQ == 0:
+                print(f"Episode {episode} saved.")
+                agent.saveModelsParams(f"models/minimalModelParams/g1{int(GAMMA*100)}e{episode}")
 
-            agent.saveCheckpoint({
-                'episode': episode+1,
-                'epsilon' : max(epsilon - EPSILON_DECAY, EPSILON_END),
-                'EPISODES' : EPISODES,
-                'SAVE_FREQ' : SAVE_FREQ,
-                'UPDATE_FREQ' : UPDATE_FREQ,
-                'SWITCH_FREQ' : SWITCH_FREQ,
-                'EPSILON_END' : EPSILON_END,
-                'EPSILON_DECAY' : EPSILON_DECAY,
-                'rngState': random.getstate(),
-                'elapsedTime' : time.time()-start,
-            },f"models/minimalModelParams/checkpoint1{int(GAMMA*100)}e{episode}")
+                agent.saveCheckpoint({
+                    'episode': episode+1,
+                    'epsilon' : max(epsilon - EPSILON_DECAY, EPSILON_END),
+                    'EPISODES' : EPISODES,
+                    'SAVE_FREQ' : SAVE_FREQ,
+                    'UPDATE_FREQ' : UPDATE_FREQ,
+                    'SWITCH_FREQ' : SWITCH_FREQ,
+                    'EPSILON_END' : EPSILON_END,
+                    'EPSILON_DECAY' : EPSILON_DECAY,
+                    'rngState': random.getstate(),
+                    'elapsedTime' : time.time()-start,
+                },f"models/minimalModelParams/checkpoint1{int(GAMMA*100)}e{episode}")
 
-        epsilon = max(epsilon - EPSILON_DECAY, EPSILON_END)
-        print("Done Game")
-    agent.saveModelsParams(f"models/minimalModelParams/g{int(GAMMA*100)}e{episode}")
-    end = time.time()
-    print(f"Training with GAMMA={GAMMA} lasted {end-start} seconds.")
+            epsilon = max(epsilon - EPSILON_DECAY, EPSILON_END)
+            print("Done Game")
+        agent.saveModelsParams(f"models/minimalModelParams/g{int(GAMMA*100)}e{episode}")
+        end = time.time()
+        print(f"Training with GAMMA={GAMMA} lasted {end-start} seconds.")
